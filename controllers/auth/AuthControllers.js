49feabfa0e4862dffa8aa515a8db0f5f8ba4ba33
IanaKarpenko <yana.karpenko.993@gmail.com>
2022-01-15 19:23:21 +0100

    added logout endpoint and the corresponding controller

--- controllers/auth/AuthControllers.js
+++ controllers/auth/AuthControllers.js
@@ -1,3 +1,15 @@
-class AuthControllers {}
+const {User} = require("../../models/users");
+const {Unauthorized} = require("http-errors");
+
+class AuthControllers {
+    async logout(req, res) {
+        const {_id} = req.user;
+        const user = await User.findByIdAndUpdate(_id, {token: null});
+        if (!user) {
+            throw new Unauthorized("Can't logout non-existing user");
+        }
+        res.status(204).json();
+    }
+}
 
 module.exports = new AuthControllers()

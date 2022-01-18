7ad2d9fb28977a5d33959b026be16c1d5104387d
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-01-18 23:05:16 +0200

    added balance changing
    added balance changing via transaction adding or removing. work in test mode

--- models/transactions.js
+++ models/transactions.js
@@ -1,6 +1,17 @@
 const { Schema, model, SchemaTypes } = require('mongoose')
 // const Joi = require('joi')
 
+const MODELS = {
+  BALANCE: 'balance',
+  TRANSACTIONS: 'transaction'
+}
+
+const TRANSACTION_TYPES = {
+  DEBIT: 'debit',
+  CREDIT: 'credit'
+}
+
+// transaction schema
 const transactionSchema = Schema({
   type: {
     type: String,
@@ -29,6 +40,47 @@ const transactionSchema = Schema({
   }
 }, { versionKey: false, timestamps: true })
 
-const Transaction = model('transaction', transactionSchema)
+// pre-hook for changing balance before adding transaction
+transactionSchema.pre('save', { document: true }, async function (next) {
+  const { owner, type, amount } = this
+
+  const balance = await model(MODELS.BALANCE)
+    .findOne({ owner: '61e71b5895d023fab1ba76e8' })
+
+  if (!balance) {
+    throw new Error('Balance not set')
+  }
+
+  balance.totalCost += type === TRANSACTION_TYPES.DEBIT ? amount : -1 * amount
+  balance.save()
+  next()
+})
+
+// pre-hook for changing balance before removing transaction
+transactionSchema.pre('findOneAndRemove', { document: false, query: true }, async function (next) {
+  const transactionId = this.getQuery()._id
+
+  const transaction = await model(MODELS.TRANSACTIONS)
+    .findOne({ _id: transactionId })
+
+  if (!transaction) {
+    next()
+  }
+
+  const { owner, type, amount } = transaction
+
+  const balance = await model(MODELS.BALANCE)
+    .findOne({ owner: '61e71b5895d023fab1ba76e8' })
+
+  if (!balance) {
+    throw new Error('Balance not set')
+  }
+
+  balance.totalCost += type === TRANSACTION_TYPES.DEBIT ? -1 * amount : amount
+  balance.save()
+  next()
+})
+
+const Transaction = model(MODELS.TRANSACTIONS, transactionSchema)
 
 module.exports = { Transaction }

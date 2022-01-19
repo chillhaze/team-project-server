5f000145d9a016e6712b49c15149e8400f9d54cd
IanaKarpenko <yana.karpenko.993@gmail.com>
2022-01-19 21:45:30 +0100

    fixed logout

--- controllers/auth/AuthControllers.js
+++ controllers/auth/AuthControllers.js
@@ -11,6 +11,16 @@ const queryString = require("query-string");
 const axios = require("axios");
 
 class AuthControllers {
+
+    async logout(req, res) {
+        const { _id } = req.user;
+        const user = await User.findByIdAndUpdate(_id, { token: null });
+        if (!user) {
+            throw new Unauthorized("Can't logout non-existing user");
+        }
+        res.status(204).json();
+    }
+
     async login(req, res) {
         const { email, password } = req.body
         const user = await User.findOne({ email })

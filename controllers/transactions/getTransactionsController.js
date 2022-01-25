72e1ed4019d722b1746228e8f5b53adf6e255b8d
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-01-25 15:59:24 +0200

    refactoring
    added ability to get all transactions per day by setting query type=all in request

--- controllers/transactions/getTransactionsController.js
+++ controllers/transactions/getTransactionsController.js
@@ -1,22 +1,30 @@
 const { Transaction } = require('../../models')
 const { getDaysInterval } = require('../../utils')
 
+const TRANSACTION_TYPES = {
+  DEBIT: 'debit',
+  CREDIT: 'credit',
+  ALL: 'all'
+}
+
 const getTransactionsPerDay = async (req, res) => {
   const { _id: owner } = req.user
   const { type, period } = req.query
-
   const [minPeriod, maxPeriod] = getDaysInterval(period)
+  const types = type === TRANSACTION_TYPES.ALL
+    ? [TRANSACTION_TYPES.DEBIT, TRANSACTION_TYPES.CREDIT]
+    : [type]
 
   const result = await Transaction
     .find({
       owner,
-      type,
+      type: { $in: types },
       completedAt: {
         $gte: new Date(minPeriod),
         $lt: new Date(maxPeriod)
       }
     })
-    .select({ owner: 0, type: 0, createdAt: 0, updatedAt: 0 })
+    .select({ owner: 0, createdAt: 0, updatedAt: 0 })
 
   res.status(200).json({
     status: 'success',
